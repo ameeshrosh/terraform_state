5ed81b8520e2d859f15ea552ecebf886fbf19c3b
ameeshrosh <ameesh_rosh@yahoo.co.in>
2024-02-16 16:18:34 -0500

    commit_v3

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "S3-bucket-terraform-state"
+    bucket = "s3-bucket-terraform-state"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamo-db-table-tf"
